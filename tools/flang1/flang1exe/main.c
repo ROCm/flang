ab8fd66c48cd3d3819099f7fb91df2abd0ace460
Shivarama Rao <Shivarama.Rao@amd.com>
2018-06-21 07:24:19 -0400

    Revert "reverting default large-arrays behavior to fix exchange2 regression"
    This reverts commit 12f470f9048277f397863078530d3f31fe8cd08b.
    
    Change-Id: I4af19ef4e2d3c0cf57ede78a0ff9231e97b572bc

--- tools/flang1/flang1exe/main.c
+++ tools/flang1/flang1exe/main.c
@@ -781,11 +781,8 @@ init(int argc, char *argv[])
   /* x flags */
   register_xflag_arg(arg_parser, "x", flg.x,
                      (sizeof(flg.x) / sizeof(flg.x[0])));
-// AOCC Begin
-// keeping the old behavior of small arrays.
   /* FIXME : temporary. Needs to be removed once the driver is updated */
-//  set_xflag(68, 1);
-// AOCC end
+  set_xflag(68, 1);
   register_yflag_arg(arg_parser, "y", flg.x);
   /* Debug flags */
   register_qflag_arg(arg_parser, "q", flg.dbg,
