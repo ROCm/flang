454f23b7a654769bdf0c91c16b29f379285d97d1
drai <Deepali.Rai@amd.com>
2019-03-06 15:07:39 +0530

    Enabling the fast math operation only under -fast-math
    Fixes CPUPC-1342
    
    Change-Id: Icba75ce52cf1f193109b2d893611168f78385816

--- tools/flang2/flang2exe/cgmain.cpp
+++ tools/flang2/flang2exe/cgmain.cpp
@@ -2702,8 +2702,7 @@ write_instructions(LL_Module *module)
       print_tmp_name(instrs->tmps);
       print_token(" = ");
       print_token(llvm_instr_names[i_name]);
-      if ((!flg.ieee || XBIT(216, 1)) &&
-        (llvm_info.curr_instr->flags & FAST_MATH_FLAG))
+      if (XBIT(216, 1))  // For fast-math
         switch (i_name) {
         case I_FADD:
           if (XBIT(216, 2))
