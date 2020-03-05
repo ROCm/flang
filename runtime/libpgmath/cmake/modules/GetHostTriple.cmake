5de8d1fe9271c05d2deb7396e622754c2ba7b472
Shivarama Rao <shivarama.rao@amd.com>
2020-03-05 12:20:08 +0530

    [CPUPC-3084] Attempt to remove the usage of config.guess file that has GPL license
    Change-Id: I5d3259f29eb602ca6340a14f3544e5c71f81307c

--- runtime/libpgmath/cmake/modules/GetHostTriple.cmake
+++ runtime/libpgmath/cmake/modules/GetHostTriple.cmake
@@ -15,15 +15,18 @@ function( get_host_triple var )
       set( value "i686-pc-mingw32" )
     endif()
   else( MSVC )
-    set(config_guess ${CMAKE_CURRENT_SOURCE_DIR}/cmake/config.guess)
-    execute_process(COMMAND sh ${config_guess}
-      RESULT_VARIABLE TT_RV
-      OUTPUT_VARIABLE TT_OUT
-      OUTPUT_STRIP_TRAILING_WHITESPACE)
-    if( NOT TT_RV EQUAL 0 )
-      message(FATAL_ERROR "Failed to execute ${config_guess}")
-    endif( NOT TT_RV EQUAL 0 )
-    set( value ${TT_OUT} )
+#
+# AOCC: removing usage of config.guess and hardcoding target-triple
+#    
+#    set(config_guess ${CMAKE_CURRENT_SOURCE_DIR}/cmake/config.guess)
+#    execute_process(COMMAND sh ${config_guess}
+#      RESULT_VARIABLE TT_RV
+#      OUTPUT_VARIABLE TT_OUT
+#      OUTPUT_STRIP_TRAILING_WHITESPACE)
+#    if( NOT TT_RV EQUAL 0 )
+#      message(FATAL_ERROR "Failed to execute ${config_guess}")
+#    endif( NOT TT_RV EQUAL 0 )
+    set( value "x86_64-unknown-linux-gnu")
   endif( MSVC )
   set( ${var} ${value} PARENT_SCOPE )
 endfunction( get_host_triple var )
