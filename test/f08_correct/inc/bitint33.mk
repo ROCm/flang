4f631c74302ee80b500944f128d7379b22e8661d
“sekalyur” <SeshaP.Kalyur@amd.com>
2020-04-10 01:25:26 +0530

    CPUPC-2991-F2008-SHIFTR produces incorrect results
    Change-Id: Ifb6ef52853890d6746db7621cc0e008ff3b757e8

--- test/f08_correct/inc/bitint33.mk
+++ test/f08_correct/inc/bitint33.mk
@@ -1,38 +1,32 @@
 #
 # Copyright (c) 2019, Advanced Micro Devices, Inc. All rights reserved.
 #
-# F2008 Compliance Tests: Stop code - Execution control
+# F2008 Compliance Tests: Intrinsics that take bit operands
 #
-# Date of Modification: Sep 10 2019
+# Date of Modification: Fri Apr 10 17:30:08 IST 2020
 #
+
 ########## Make rule for test bitint33  ########
 
+
 bitint33: bitint33.run
-
-#include ./passok.mk
-
-passok.$(OBJX):  $(SRC)/passok.f08
-	-$(RM) passok.$(EXESUFFIX) core *.d *.mod FOR*.DAT FTN* ftn* fort.*
-	@echo ------------------------------------ building test $@
-	-$(CC) -c $(CFLAGS) $(SRC)/check.c -o check.$(OBJX)
-	-$(FC) -c $(FFLAGS) $(LDFLAGS) $(SRC)/passok.f08 -o passok.$(OBJX)
-	-$(FC) $(FFLAGS) $(LDFLAGS) passok.$(OBJX) check.$(OBJX) $(LIBS) -o passok.$(EXESUFFIX)
 
 bitint33.$(OBJX):  $(SRC)/bitint33.f08
 	-$(RM) bitint33.$(EXESUFFIX) core *.d *.mod FOR*.DAT FTN* ftn* fort.*
 	@echo ------------------------------------ building test $@
-	#-$(CC) -c $(CFLAGS) $(SRC)/check.c -o check.$(OBJX)
-	-$(FC) -c $(FFLAGS) $(LDFLAGS) $(SRC)/bitint33.f08 -o bitint33.$(OBJX) ||:
-	#-$(FC) $(FFLAGS) $(LDFLAGS) bitint33.$(OBJX) check.$(OBJX) $(LIBS) -o bitint33.$(EXESUFFIX) ||:
+	-$(CC) -c $(CFLAGS) $(SRC)/check.c -o check.$(OBJX)
+	-$(FC) -c $(FFLAGS) $(LDFLAGS) $(SRC)/bitint33.f08 -o bitint33.$(OBJX)
+	-$(FC) $(FFLAGS) $(LDFLAGS) bitint33.$(OBJX) check.$(OBJX) $(LIBS) -o bitint33.$(EXESUFFIX)
 
-bitint33.run: passok.$(OBJX)
+
+bitint33.run: bitint33.$(OBJX)
 	@echo ------------------------------------ executing test bitint33
-	-passok.$(EXESUFFIX) ||:
+	bitint33.$(EXESUFFIX)
 
 build:	bitint33.$(OBJX)
 
 verify:	;
 
-run:	 passok.$(OBJX)
+run:	 bitint33.$(OBJX)
 	@echo ------------------------------------ executing test bitint33
-	-passok.$(EXESUFFIX) ||:
+	-bitint33.$(EXESUFFIX) ||:
