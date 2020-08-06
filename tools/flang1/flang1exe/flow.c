7fed59e1d9e95ecd2173ca32a624a0025da1e910
Shivarama Rao <shivarama.rao@amd.com>
2020-08-06 17:15:50 +0530

    [CPUPC-3798] Increasing the thresold limit for stb creation to resolve wrf compilation issues
    Change-Id: I068266fc43790f99b1dfe7eef727078e917c2dfa

--- tools/flang1/flang1exe/flow.c
+++ tools/flang1/flang1exe/flow.c
@@ -1796,7 +1796,7 @@ new_storeitem(int nme)
   int store;
 
   store = opt.storeb.stg_avail++;
-  if (store > 32767)
+  if (store > 32767 * 4)  // AOCC increasing limit
     error(7, 4, 0, CNULL, CNULL);
   OPT_NEED(opt.storeb, STORE, 100);
   STORE_TYPE(store) = 0;
@@ -1828,7 +1828,7 @@ update_stl(int lpx, int nme)
     if (STORE_NM(store) == nme)
       return store;
   store = opt.storeb.stg_avail++;
-  if (store > 32767)
+  if (store > 65535)
     error(7, 4, 0, CNULL, CNULL);
   OPT_NEED(opt.storeb, STORE, 100);
   STORE_TYPE(store) = 0;
@@ -2761,7 +2761,7 @@ const_prop(void)
 
   if (XBIT(6, 0x1))
     return FALSE;
-  return false;
+//  return false;
 #if DEBUG
   if (OPTDBG(9, 32768)) {
     fprintf(gbl.dbgfil, "const_prop: file %s, function %s\n", gbl.src_file,
