7f2ba2dc8d3bc71f8f34f70d2fb66926072c81fb
Shivarama Rao <shivarama.rao@amd.com>
2020-09-06 09:23:29 +0530

    [CPUPC-4071][CPUPC-4164] QUAD support for SIND, COSD,TAND intrinsics
    Change-Id: Id1ea942098d6769d3ca834d0776d091b1ff22dfb

---
+++ runtime/libpgmath/lib/common/qtand.c
@@ -0,0 +1,17 @@
+/*
+ * Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+ * See https://llvm.org/LICENSE.txt for license information.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ *
+ */
+
+#include "mthdecls.h"
+#include <quadmath.h>
+
+__float128 __attribute__((weak)) tanq( __float128);
+
+__float128
+__mth_i_qtand(__float128 q)
+{
+  return (tanq(CNVRTDEG(q)));
+}
