9f522c736a179663d104517e7fb2b34912407b30
aputhanp <abinav.puthanpurayil@amd.com>
2020-09-07 14:11:36 +0530

    [CPUPC-4158] real128 support for norm2
    Change-Id: I05e7bb61179bc6a81cfe8e4f88a440afd803a0c5

--- runtime/flang/red_norm2.c
+++ runtime/flang/red_norm2.c
@@ -17,6 +17,12 @@ void F90_I8(stride_1_norm2_real8) (__POINT_T *src_pointer, __INT_T *size, __REAL
     F90_I8(norm2_real8) (src_pointer, size, result);
 }
 
+// AOCC begin
+void F90_I8(stride_1_norm2_real16) (__POINT_T *src_pointer, __INT_T *size, __REAL16_T *result) {
+    F90_I8(norm2_real16) (src_pointer, size, result);
+}
+// AOCC end
+
 void ENTFTN(NORM2_NODIM, norm2_nodim) (__POINT_T *result, __POINT_T *src, __INT4_T * pfr, _DIST_TYPE *result_kind, F90_Desc *src_desc) {
   char error_msg[50];
 
@@ -74,6 +80,35 @@ void ENTFTN(NORM2_NODIM, norm2_nodim) (__POINT_T *result, __POINT_T *src, __INT4
         sprintf(error_msg, "Rank : %d, can not be less than 1 or greater 7\n", src_desc->rank);
         __fort_abort(error_msg);
     }
+  // AOCC begin
+  } else if (src_desc->kind == __REAL16) {
+    switch(src_desc->rank) {
+      case 1:
+        F90_NORM2(nodim_1_real16) (result, src, src_desc);
+        break;
+      case 2:
+        F90_NORM2(nodim_2_real16) (result, src, src_desc);
+        break;
+      case 3:
+        F90_NORM2(nodim_3_real16) (result, src, src_desc);
+        break;
+      case 4:
+        F90_NORM2(nodim_4_real16) (result, src, src_desc);
+        break;
+      case 5:
+        F90_NORM2(nodim_5_real16) (result, src, src_desc);
+        break;
+      case 6:
+        F90_NORM2(nodim_6_real16) (result, src, src_desc);
+        break;
+      case 7:
+        F90_NORM2(nodim_7_real16) (result, src, src_desc);
+        break;
+      default:
+        sprintf(error_msg, "Rank : %d, can not be less than 1 or greater 7\n", src_desc->rank);
+        __fort_abort(error_msg);
+    }
+  // AOCC end
   } else if (src_desc->len == 0) {
     // empty array case
     *result = 0.0;
@@ -144,6 +179,35 @@ void ENTFTN(NORM2, norm2) (__POINT_T *result, __POINT_T *src, __INT4_T * pfr, __
         sprintf(error_msg, "Rank : %d, can not be less than 1 or greater 7\n", src_desc->rank);
         __fort_abort(error_msg);
     }
+  // AOCC begin
+  } else if (src_desc->kind == __REAL16) {
+    switch (src_desc->rank) {
+      case 1:
+        F90_NORM2(nodim_1_real16) (result, src, src_desc);
+        break;
+      case 2:
+        F90_NORM2(dim_2_real16) (result, src, dim, result_desc, src_desc);
+        break;
+      case 3:
+        F90_NORM2(dim_3_real16) (result, src, dim, result_desc, src_desc);
+        break;
+      case 4:
+        F90_NORM2(dim_4_real16) (result, src, dim, result_desc, src_desc);
+        break;
+      case 5:
+        F90_NORM2(dim_5_real16) (result, src, dim, result_desc, src_desc);
+        break;
+      case 6:
+        F90_NORM2(dim_6_real16) (result, src, dim, result_desc, src_desc);
+        break;
+      case 7:
+        F90_NORM2(dim_7_real16) (result, src, dim, result_desc, src_desc);
+        break;
+      default:
+        sprintf(error_msg, "Rank : %d, can not be less than 1 or greater 7\n", src_desc->rank);
+        __fort_abort(error_msg);
+    }
+  // AOCC end
   } else if (src_desc->len == 0) {
     // empty array case
     *result = 0.0;
