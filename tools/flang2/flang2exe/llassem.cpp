f6d2da5ca8fcee648f6154966e3d5dc7d67c1516
Shivarama Rao <shivarama.rao@amd.com>
2020-12-13 17:08:34 +0530

    [CPUPC-4945] Fixing the issues in calling finalize functions
    Change-Id: I34ec6946bf12f1e7d63dc78085a298b76893c505

--- tools/flang2/flang2exe/llassem.cpp
+++ tools/flang2/flang2exe/llassem.cpp
@@ -2172,7 +2172,7 @@ write_parent_pointers(int parent, int level)
 /* final table size is max dimensions plus 2. The 0th element holds the
  * scalar subroutine and the last element holds the elemental subroutine.
  */
-#define FINAL_TABLE_SZ 9
+#define FINAL_TABLE_SZ MAXSUBS + 2
 
 static int
 build_final_table(DTYPE dtype, SPTR ft[FINAL_TABLE_SZ])
