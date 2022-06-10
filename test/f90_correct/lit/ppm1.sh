2867b7acf8d66afbb72f41e6122ee2959fc0705b
Lynd Stringer <Lynd.Stringer@amd.com>
2022-06-10 13:39:39 -0500

    Corrected the AMD Copyright Statement Notice.
    Change-Id: I1ee997aa3d636a493526799aaed644abe40d9587

--- test/f90_correct/lit/ppm1.sh
+++ test/f90_correct/lit/ppm1.sh
@@ -1,7 +1,4 @@
-# Copyright (c) 2019, Advanced Micro Devices, Inc. All rights reserved.
-#
-# Date of Modification: December 2019
-#
+# Copyright(C) 2019 Advanced Micro Devices, Inc. All rights reserved.
 
 # RUN: KEEP_FILES=%keep FLAGS=%flags TEST_SRC=%s MAKE_FILE_DIR=%S/.. bash %S/runmake | tee %t 
 # RUN: cat %t | FileCheck %S/runmake
