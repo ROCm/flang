58991e0b2c67a6a82172d10d81d91e4d57002030
estewart08 <ethan.stewart@amd.com>
2024-11-11 16:16:42 -0600

    [flang-legacy] - Properly use LIBRARY_PATH if provided (#89)
    If LIBRARY_PATH is not a valid path for the bitcode libraries
    ensure that a fallback location is used. For RPATH we check to
    see if the path is valid before adding it along with known good
    default locations.
    
    Removed FindDebugPerfInLibraryPath function.

--- flang-legacy/17.0-4/llvm-legacy/clang/lib/Driver/ToolChains/Gnu.cpp
+++ flang-legacy/17.0-4/llvm-legacy/clang/lib/Driver/ToolChains/Gnu.cpp
@@ -568,8 +568,10 @@ void tools::gnutools::Linker::ConstructJob(Compilation &C, const JobAction &JA,
   // Add Fortran runtime libraries
   if (needFortranLibs(D, Args)) {
     ToolChain.AddFortranStdlibLibArgs(Args, CmdArgs);
-    CmdArgs.push_back("-rpath");
-    CmdArgs.push_back(Args.MakeArgString(D.Dir + "/../lib"));
+    if (!Args.hasArg(options::OPT_fopenmp)){
+      CmdArgs.push_back("-rpath");
+      CmdArgs.push_back(Args.MakeArgString(D.Dir + "/../lib"));
+    }
   } else {
     // Claim "no Flang libraries" arguments if any
     for (auto Arg : Args.filtered(options::OPT_noFlangLibs)) {
